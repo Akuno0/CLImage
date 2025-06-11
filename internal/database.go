62ece9e3c3e699d1bbf9d7ffadd2c0710b5cb570
Akun0 <somebody1541@gmail.com>
2025-06-11 19:00:02 +0300

    Немного для бд

--- internal/database.go
+++ internal/database.go
@@ -1,6 +1,22 @@
 // Для работы с БД
 package internal
 
-func db() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
 
+func db() {
+	connStr := "user=user password=123 dbname=db host=localhost port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Подключено")
 }
